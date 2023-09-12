e0a6380c75dcec59801da434c12de3478da9f285
abracoder <abrakilvis@gmail.com>
2023-09-12 18:12:37 +0530

    learned about passing props from child to parents and vice -versa

--- src/App.jsx
+++ src/App.jsx
@@ -3,72 +3,42 @@ import './App.css'
 import './Style.css'
 
 
-const user = {
-  name : " suraj Gupta ",
-  imageUrl : 'https://i.imgur.com/yXOvdOSs.jpg',
-  imazeSize : 101,
-}
+function Board(){
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
-const products = [
-  { title: 'Cabbage', id: 1 },
-  { title: 'Garlic', id: 2 },
-  { title: 'Apple', id: 3 },
-];
-let guest = 0;
-
-function Cup() {
-  // Bad: changing a preexisting variable!
-  guest = guest + 1;
-  return <h2>Tea cup for guest #{guest}</h2>;
-}
-
-
-export default function App () {
-  const [count, setCount] = useState(0);
-
-  function handleClick(){
-    console.log("button clicked" + count)
-    setCount(count +1)
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
   }
-
-   return (
-    <>
-    <MyImageProfile/>
-      <p> HELLO React</p>
-      <Cup />
-      <Cup />
-      <Cup />
-      <MyButton  count = { count } handleClick = {handleClick}/>
-      <MyButton  count = { count } handleClick = {handleClick}/>
-
-      </>
-   )
-}
-
-
-function MyImageProfile(){
-  const productItems = products.map (product  => {
-    return (
-      <>
-        <h1>{product.title}</h1>
-        <img src ={user.imageUrl} 
-        alt = "profile photo"
-        style = {{ width: user.imazeSize,
-        height: user.imazeSize}}></img>    
-      </>
-     )
-
-  })
-  return productItems;
-   
-}
-
-function MyButton({ count, handleClick }){
-
   return (
-    <button className = " myButton"  onClick = {handleClick}>
-      MyButton clicked count = {count}
-    </button>
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
   )
 }
 
+function Square ( {value, onSquareClick} ){
+  return <button className = "square" onClick={onSquareClick} >{value}</button>
+}
+
+
+export default function App(){
+  return (
+  <Board />
+  )
+}
