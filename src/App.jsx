dad3af5a8b91ee86c7308c91cca052a874952567
abracoder <abrakilvis@gmail.com>
2023-09-12 15:39:20 +0530

    first commit while learning React from the documentation

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import './App.css'
+import './Style.css'
+
+
+const user = {
+  name : " suraj Gupta ",
+  imageUrl : 'https://i.imgur.com/yXOvdOSs.jpg',
+  imazeSize : 101,
+}
+
+const products = [
+  { title: 'Cabbage', id: 1 },
+  { title: 'Garlic', id: 2 },
+  { title: 'Apple', id: 3 },
+];
+
+
+export default function App () {
+  const [count, setCount] = useState(0);
+
+  function handleClick(){
+    console.log("button clicked" + count)
+    setCount(count +1)
+  }
+
+   return (
+    <>
+    <MyImageProfile/>
+      <p> HELLO React</p>
+      <MyButton  count = { count } handleClick = {handleClick}/>
+      <MyButton  count = { count } handleClick = {handleClick}/>
+      </>
+   )
+}
+
+
+function MyImageProfile(){
+  const productItems = products.map (product  => {
+    return (
+      <>
+        <h1>{product.title}</h1>
+        <img src ={user.imageUrl} 
+        alt = "profile photo"
+        style = {{ width: user.imazeSize,
+        height: user.imazeSize}}></img>    
+      </>
+     )
+
+  })
+  return productItems;
+   
+}
+
+function MyButton({ count, handleClick }){
+
+  return (
+    <button className = " myButton"  onClick = {handleClick}>
+      MyButton clicked count = {count}
+    </button>
+  )
+}
+
